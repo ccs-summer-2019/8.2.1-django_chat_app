e4bb9dd95603160fea7f23e1d2b681949dd85920
MC3D <m.chapman.home@gmail.com>
2019-10-16 15:51:58 -0400

    updates accounts admin

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,8 +1,13 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
-from .models import Profile, User
+from .forms import CustomUserCreationForm, CustomUserChangeForm
+from .models import CustomUser
 
+class CustomUserAdmin(UserAdmin):
+    add_form = CustomUserCreationForm
+    form = CustomUserChangeForm
+    model = CustomUser
+    list_display = ['email', 'username',]
 
-admin.site.register(User)
-
-admin.site.register(Profile)
+admin.site.register(CustomUser, CustomUserAdmin)
