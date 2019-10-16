664b7ea96c706e0a6c3bfa3111cc4563281d2526
MC3D <m.chapman.home@gmail.com>
2019-10-16 15:33:21 -0400

    updates for signup

--- accounts/views.py
+++ accounts/views.py
@@ -1,12 +1,12 @@
-from django.contrib.auth.forms import UserCreationForm
 from django.urls import reverse_lazy
 from django.views import generic
 
+from .forms import CustomUserCreationForm
 from .models import Profile
 
 
 class SignUpView(generic.CreateView):
-    form_class = UserCreationForm
+    form_class = CustomUserCreationForm
     success_url = reverse_lazy('login')
     template_name= 'signup.html'
 
